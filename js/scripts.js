99d49d470d1afedf0d4efe47e7ce637ea7822504
alonso ramirez <alonsoramirezdepablos@hotmail.com>
2022-07-12 19:37:26 -0500

    SEDISA 2

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@
 
 
     /* Image Slider - Swiper */
-    var imageSlider = new Swiper('.image-slider', {
+    var imageSlider = new Swiper('.image-slider1', {
         autoplay: {
             delay: 2000,
             disableOnInteraction: false
@@ -68,6 +68,39 @@
             992: {
                 slidesPerView: 3,
                 spaceBetween: 20
+            },
+            // when window is <= 1200px
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 20
+            },
+
+        }
+    });
+
+      /* Image Slider - Swiper */
+      var imageSlider = new Swiper('.image-slider', {
+       
+        
+        spaceBetween: 50,
+        slidesPerView: 'auto',
+        loopedSlides: 5,
+        
+		breakpoints: {
+            // when window is <= 580px
+            580: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window is <= 768px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window is <= 992px
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 30
             },
             // when window is <= 1200px
             1200: {
